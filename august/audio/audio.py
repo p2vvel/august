69e5aed479036a46862feef3cba6a9d04a1d1f8a
p2vvel <p2vvel@gmail.com>
2023-11-04 00:24:45 +0100

    Prepare audio augmentation class

--- august/audio/audio.py
+++ august/audio/audio.py
@@ -1,8 +1,42 @@
+from numpy import ndarray
+
 from . import utils
 import librosa
-import random
 
 
 class AugustAudio:
-    def __init__(self, audio_path: str):
+    def __init__(self, audio_path: str) -> None:
+        self.y: ndarray
+        self.sr: int
         self.y, self.sr = librosa.load(audio_path)
+        self.sr = int(self.sr)
+
+    def time_shift(self, min_shift: float = -0.5, max_shift: float = 0.5, p: float = 0.5) -> None:
+        self.y = utils.time_shift(self.y, self.sr, min_shift=min_shift, max_shift=max_shift, p=p)
+
+    def time_stretch(self, min_factor: float = 0.5, max_factor: float = 1.5, p: float = 0.5) -> None:
+        self.y = utils.time_stretch(self.y, self.sr, min_factor=min_factor, max_factor=max_factor, p=p)
+
+    def invert_polarity(self, p: float = 0.5) -> None:
+        self.y = utils.invert_polarity(self.y, self.sr, p=p)
+
+    def pitch_scale(self, min_semitones: int = -6, max_semitones: int = 6, p: float = 0.5) -> None:
+        self.y = utils.pitch_scale(self.y, self.sr, min_semitones=min_semitones, max_semitones=max_semitones, p=p)
+
+    def random_gain(self, min_factor: float = 0.5, max_factor: float = 1.5, p: float = 0.5) -> None:
+        self.y = utils.random_gain(self.y, self.sr, min_factor=min_factor, max_factor=max_factor, p=p)
+
+    def gaussian_noise(self, min_amplitude: float = 0.001, max_amplitude: float = 0.015, p: float = 0.5) -> None:
+        self.y = utils.gaussian_noise(self.y, min_amplitude=min_amplitude, max_amplitude=max_amplitude, p=p)
+
+    def time_mask(self, min_part: float = 0.01, max_part: float = 0.5, p: float = 0.5) -> None:
+        self.y = utils.time_mask(self.y, self.sr, min_part=min_part, max_part=max_part, p=p)
+
+    def low_pass_filter(self, min_freq: float = 150, max_freq: float = 7500, p: float = 0.5) -> None:
+        self.y = utils.low_pass_filter(self.y, self.sr, min_freq=min_freq, max_freq=max_freq, p=p)
+
+    def high_pass_filter(self, min_freq: float = 20, max_freq: float = 2400, p: float = 0.5) -> None:
+        self.y = utils.high_pass_filter(self.y, self.sr, min_freq=min_freq, max_freq=max_freq, p=p)
+
+    def room(self, p: float = 0.5) -> None:
+        self.y = utils.room(self.y, self.sr, p=p)
--- august/audio/utils.py
+++ august/audio/utils.py
@@ -1,7 +1,9 @@
+import librosa
 from pydub import AudioSegment
 from pydub.playback import play
 import random
 import numpy as np
+from numpy import ndarray
 from audiomentations import (
     Compose,
     AddGaussianNoise,
@@ -13,71 +15,88 @@ from audiomentations import (
     RoomSimulator
 )
 
-# Time shifting
+
+# TODO: add util to download echoes and implement impulse response
+def impulse_response(y: ndarray, sr: int):
+    convert = Compose([ApplyImpulseResponse()])
+    return convert(samples=y, sample_rate=sr)
 
 
-def time_shift(y, sr):
-    convert = Compose([Shift()])
+def _invert_polarity(y: ndarray) -> ndarray:
+    return y * -1
+
+
+def _time_stretch(y: ndarray, sr: int, factor: float) -> ndarray:
+    return librosa.effects.time_stretch(y, rate=factor)
+
+
+def time_shift(y: ndarray, sr: int, *, min_shift: float, max_shift: float, p: float) -> ndarray:
+    convert = Compose([Shift(min_shift=min_shift, max_shift=max_shift, p=p)])
     y_new = convert(samples=y, sample_rate=sr)
     return y_new
 
 
-def _time_stretch(y, sr, factor: float):
-    new_y = librosa.effects.time_stretch(y, rate=factor)
-    return new_y
+def time_stretch(y: ndarray, sr: int, *, min_factor: float, max_factor: float, p: float) -> ndarray:
+    if random.random() < p:
+        factor = random.uniform(min_factor, max_factor)
+        return _time_stretch(y, sr, factor=factor)
+    return y
 
 
-def _pitch_scale(signal, sr, num_semitones):
-    return librosa.effects.pitch_shift(signal, sr=sr, n_steps=num_semitones)
+def invert_polarity(y: ndarray, sr: int, *, p: float) -> ndarray:
+    if random.random() < p:
+        return _invert_polarity(y)
+    return y
 
 
-def _random_gain(signal, min_factor, max_factor):
-    gain_rate = random.uniform(min_factor, max_factor)
-    augmented_signal = signal * gain_rate
-    return augmented_signal
+def _pitch_scale(y: ndarray, sr: int, num_semitones: float) -> ndarray:
+    return librosa.effects.pitch_shift(y, sr=sr, n_steps=num_semitones)
 
 
-def invert_polarity(signal):
-    return signal * -1
+def pitch_scale(y: ndarray, sr: int, *, min_semitones: int, max_semitones: int, p: float) -> ndarray:
+    if random.random() < p:
+        num_semitones = random.randint(min_semitones, max_semitones)
+        return _pitch_scale(y, sr, num_semitones=num_semitones)
+    return y
 
 
-def gaussian_noise(signal, sr):
-    convert = Compose([AddGaussianNoise()])
-    y_new = convert(samples=signal, sample_rate=sr)
-    return y_new
+def _gain(y: ndarray, factor: float) -> ndarray:
+    return y * factor
 
 
-def impulse_response(signal, sr):
-    convert = Compose([ApplyImpulseResponse()])
-    y_new = convert(samples=signal, sample_rate=sr)
-    return y_new
+def random_gain(y: ndarray, sr: int, *, min_factor: float, max_factor: float, p: float) -> ndarray:
+    if random.random() < p:
+        gain_rate = random.uniform(min_factor, max_factor)
+        return _gain(y, gain_rate)
+    return y
 
 
-def time_mask(signal, sr):
-    convert = Compose([TimeMask()])
-    y_new = convert(samples=signal, sample_rate=sr)
-    return y_new
+def gaussian_noise(y: ndarray, sr: int, *, min_amplitude: float, max_amplitude: float, p: float) -> ndarray:
+    convert = Compose([AddGaussianNoise(min_amplitude=min_amplitude, max_amplitude=max_amplitude, p=p)])
+    return convert(samples=y, sample_rate=sr)
 
 
-def low_pass_filter(signal, sr):
-    convert = Compose([LowPassFilter()])
-    y_new = convert(signal, sample_rate=sr)
-    return y_new
+def time_mask(y: ndarray, sr: int, *, min_part: float, max_part: float, p: float) -> ndarray:
+    convert = Compose([TimeMask(min_band_part=min_part, max_band_part=max_part, p=p)])
+    return convert(samples=y, sample_rate=sr)
 
 
-def high_pass_filter(signal, sr):
-    convert = Compose([HighPassFilter()])
-    y_new = convert(signal, sample_rate=sr)
-    return y_new
-
-def room(signal, sr):
-    convert = Compose([RoomSimulator()])
-    y_new = convert(signal, sample_rate=sr)
-    return y_new
+def low_pass_filter(y: ndarray, sr: int, *, min_freq: float, max_freq: float, p: float) -> ndarray:
+    convert = Compose([LowPassFilter(min_cutoff_freq=min_freq, max_cutoff_freq=max_freq, p=p)])
+    return convert(y, sample_rate=sr)
 
 
+def high_pass_filter(y: ndarray, sr: int, *, min_freq: float, max_freq: float, p: float) -> ndarray:
+    convert = Compose([HighPassFilter(min_cutoff_freq=min_freq, max_cutoff_freq=max_freq, p=p)])
+    return convert(y, sample_rate=sr)
 
-###### https://youtu.be/bm1cQfb_pLA?si=8FGGSA3zPCxUseGJ
+
+def room(y: ndarray, sr: int, *, p: float) -> ndarray
+    convert = Compose([RoomSimulator(p=p)])
+    return convert(y, sample_rate=sr)
+
+
+# https://youtu.be/bm1cQfb_pLA?si=8FGGSA3zPCxUseGJ
 
 
 def librosa_play(y, sr):
@@ -87,26 +106,3 @@ def librosa_play(y, sr):
         temp.tobytes(), frame_rate=sr, sample_width=temp.dtype.itemsize, channels=1
     )
     play(audio_segment)
-
-
-if __name__ == "__main__":
-    from pathlib import Path
-
-    base_path = Path("C:\\Users\\p2vve\\PycharmProjects\\august\\august\\audio\\tests\\")
-    path = base_path / "bff.m4a"
-
-
-    import librosa
-
-    y, sr = librosa.load(path)
-    # y = time_shift(y, sr)
-    y = room(y, sr)
-    y = high_pass_filter(y, sr)
-
-    # y = time_stretch(y, sr, 0.6)
-    # y = pitch_scale(y, sr, -3)
-    # y = invert_polarity(y)
-    # y = random_gain(y, min_factor=1.5, max_factor=2)
-
-    # Audio(y, rate=sr)
-    librosa_play(y, sr)
