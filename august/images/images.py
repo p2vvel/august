1d9a5305da4edac018ca3415734c5ac5b4a8bc78
p2vvel <p2vvel@gmail.com>
2023-11-05 16:00:24 +0100

    Use config in ImageAugust

--- august/images/images.py
+++ august/images/images.py
@@ -22,65 +22,53 @@ class AugustImage(AugustImageMixin, ExecuteAugmentationMixin):
         self.img.show(title=title)
 
     @mark_augmentation
-    def mirror(self, p: float = 0.5) -> None:
-        if random.random() <= p:
+    def mirror(self) -> None:
+        if random.random() <= self.config.mirror_p:
             self._mirror()
 
     @mark_augmentation
-    def flip(self, p: float = 0.5) -> None:
-        if random.random() <= p:
+    def flip(self) -> None:
+        if random.random() <= self.config.flip_p:
             self._flip()
 
     @mark_augmentation
-    def color_change(self, p: float = 0.5) -> None:
-        if random.random() <= p:
+    def color_change(self) -> None:
+        if random.random() <= self.config.color_p:
             color_foo = random.choice((self._sepia, self._black_and_white))
             color_foo()
 
     @mark_augmentation
-    def color_temperature(self, p: float = 0.5, ratio_min: int = -50, ratio_max: int = 50) -> None:
-        if random.random() <= p:
-            ratio = random.randint(ratio_min, ratio_max)
+    def color_temperature(self) -> None:
+        if random.random() <= self.config.temperature_p:
+            ratio = random.randint(self.config.min_temperature_ratio, self.config.max_temperature_ratio)
             self._color_temperature(ratio)
 
     @mark_augmentation
-    def rotate(self, p: float = 0.5, angle_min: int = -89, angle_max: int = 89) -> None:
-        if random.random() <= p:
-            angle = random.randint(angle_min, angle_max)
+    def rotate(self) -> None:
+        if random.random() <= self.config.rotate_p:
+            angle = random.randint(self.config.min_angle, self.config.max_angle)
             self._rotate(angle)
 
     @mark_augmentation
-    def blur(self, p: float = 0.5, pixel_radius_min: int = 1, pixel_radius_max: int = 5) -> None:
-        if random.random() <= p:
-            pixel_radius = random.randint(pixel_radius_min, pixel_radius_max)
+    def blur(self) -> None:
+        if random.random() <= self.config.blur_p:
+            pixel_radius = random.randint(self.config.min_pixel_radius, self.config.max_pixel_radius)
             self._blur(pixel_radius)
 
     @mark_augmentation
-    def offset(
-        self,
-        p: float = 0.5,
-        x_offset_min: float = -0.5,
-        x_offset_max: float = 0.5,
-        y_offset_min: float = -0.5,
-        y_offset_max: float = 0.5,
-    ) -> None:
-        if random.random() <= p:
+    def offset(self) -> None:
+        if random.random() <= self.config.offset_p:
             x, y = self.img.size
-            x_offset = random.uniform(x_offset_min, x_offset_max)
-            y_offset = random.uniform(y_offset_min, y_offset_max)
+            x_offset = random.uniform(self.config.min_x_offset, self.config.max_x_offset)
+            y_offset = random.uniform(self.config.min_y_offset, self.config.max_y_offset)
             self._offset(int(x_offset * x), int(y_offset * y))
 
     @mark_augmentation
-    def crop(
-        self,
-        p: float = 0.5,
-        min_width: float = 0.5,
-        max_width: float = 0.9,
-        min_height: float = 0.5,
-        max_height=0.9,
-    ) -> None:
-        if random.random() <= p:
+    def crop(self) -> None:
+        if random.random() <= self.config.crop_p:
             width, height = self.img.size
+            min_width, max_width = self.config.min_x_crop, self.config.max_x_crop
+            min_height, max_height = self.config.min_y_crop, self.config.max_y_crop
             crop_width = random.randint(int(min_width * width), int(max_width * width))
             crop_height = random.randint(int(min_height * height), int(max_height * height))
             x_min = random.randint(0, width - crop_width)
